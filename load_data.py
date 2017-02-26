1a64c32a51c84b96380bf8c2f4a32059101470d3
Daksh Gupta <dakshkumargupta@gmail.com>
2017-02-25 18:37:27 -0600

    created load_data

---
+++ load_data.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pickle
+import os
+
+def loadData(dir):
+	pTime = []
+	pLatLong = []
+	pAlt = []
+	pVelocity = []
+	lData = []
+	if not os.path.exists('pTime.pckl'):
+		pTime = np.loadtxt(dir+'/Partition6467ProbePoints.csv',dtype=str,delimiter=',',usecols=(0,1))
+		pLatLong = np.loadtxt(dir+'/Partition6467ProbePoints.csv',dtype=str,delimiter=',',usecols=(0,3,4))
+		pAlt = np.loadtxt(dir+'/Partition6467ProbePoints.csv',dtype=str,delimiter=',',usecols=(0,5))
+		pVelocity = np.loadtxt(dir+'/Partition6467ProbePoints.csv',dtype=str,delimiter=',',usecols=(0,6,7))
+		pickle.dump(pTime,open('pTime.pckl','wb'))
+		pickle.dump(pLatLong,open('pLatLong.pckl','wb'))
+		pickle.dump(pAlt,open('pAlt.pckl','wb'))
+		pickle.dump(pVelocity,open('pVelocity.pckl','wb'))
+	else:
+		# vars = [pTime, pLatLong, pAlt, pVelocity]
+		# files = ['pTime.pckl', 'pLatLong.pckl', 'pAlt.pckl', 'pVelocity.pckl']
+		pTime = pickle.load(open('pTime.pckl','rb'))
+		pLatLong = pickle.load(open('pLatLong.pckl','rb'))
+		pAlt = pickle.load(open('pAlt.pckl','rb'))
+		pVelocity = pickle.load(open('pVelocity.pckl','rb'))
+		# for i,j in zip(vars, files):
+			# i = pickle.load(open(j,'rb'))
+	# if not os.path.exists('lData.pckl'):
+	# 	lData = np.loadtxt(dir+'/Partition6467LinkData.csv',dtype=str,delimiter=',')
+	# 	pickle.dump(lData,open('lData.pckl','wb'))
+	# else:
+	# 	lData = pickle.load(open('lData.pckl','rb'))
+	return pTime, pLatLong, pAlt, pVelocity
+
+if __name__ == '__main__':
+	x,y = loadData('probe_data_map_matching')[:2]
+	# print(x.shape)
+	# print(y.shape)
+	print(x[1,1])
