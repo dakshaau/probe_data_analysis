127f25884c2ead54d31d4bfd93ffbff67d618614
Daksh Gupta <dakshkumargupta@gmail.com>
2017-02-26 20:37:32 -0600

    created graph; functions: loadLink, fixString

--- load_data.py
+++ load_data.py
@@ -1,14 +1,33 @@
 import numpy as np
 import pickle
 import os
+from collections import defaultdict
 # import time
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 def loadTime(dir):
 	x,y = np.loadtxt(dir+'/Partition6467ProbePoints.csv', dtype=str, delimiter=',', usecols=(0,1), unpack=True)
 	y = np.array([datetime.strptime(y[i], "b'%m/%d/20%y %I:%M:%S %p'") for i in range(y.shape[0])])
 	return x,y
 
+def fixString(x):
+	return x.split("'")[-2]
+
+def loadLink(dir):
+	x,y,z,cat = np.loadtxt(dir+'/Partition6467LinkData.csv', dtype=str, delimiter=',', usecols=(0,1,2,5), unpack=True)
+	graph = defaultdict(lambda: [])
+	for i in range(y.shape[0]):
+		Y = fixString(y[i])
+		Z = fixString(z[i])
+		if cat[i] == "b'F'":
+			graph[Y].append(Z)
+		elif cat[i] == "b'T'":
+			graph[Y].append(Z)
+		elif cat[i] == "b'B'":
+			graph[Y].append(Z)
+			graph[Z].append(Y)
+		# graph[y[i]].append(z[i])
+	return graph
 
 def loadData(dir):
 	pTime = []
@@ -46,6 +65,20 @@ if __name__ == '__main__':
 	# print(x.shape)
 	# print(y.shape)
 	# print(x[1,1])
-	ID, date_time = loadTime('probe_data_map_matching')
-	t = date_time[10]
-	print(t.isoformat(' '))
+
+	# ID, date_time = loadTime('probe_data_map_matching')
+	# t = date_time[10]
+	# ten_min = timedelta(minutes=10)
+	# print(t.isoformat(' '))
+	# t = t+ten_min
+	# print(t.isoformat(' '))
+
+	graph = loadLink('probe_data_map_matching')
+	# for i,x in enumerate(graph.items()):
+	# 	if i==10:
+	# 		break
+	# 	k,v = x
+	# 	print('{}: {}'.format(k,v))
+	print(len(graph))
+	# print(graph["162844982"])
+	# print(fixString("b'16244982'"))
