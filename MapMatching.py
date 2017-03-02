640e2142b3708371574251f57a905fd6cb7a8af4
Daksh Gupta <dakshkumargupta@gmail.com>
2017-03-02 16:11:24 -0600

    Added Trajectory Tracking Procedure to MapMatching.py

--- MapMatching.py
+++ MapMatching.py
@@ -31,13 +31,22 @@ def calculateHE(P1, P2, heading):
 	pi = np.pi
 	delta = np.subtract(P2,P1)
 	delta *= pi/180.
+	# print(delta)
 	theta = None
 	if delta[0] < 0 and delta[1] >= 0:
-		theta = (2.5*pi - arctan(delta[1]/delta[0])) * 180/pi
+		temp = -1.*pi/2.
+		if delta[0] != 0 :
+			temp = arctan(delta[1]/delta[0])
+		theta = (2.5*pi - temp) * 180/pi
 	else:
-		theta = (2.5*pi - arctan(delta[1]/delta[0])) * 180/pi
-
+		temp = pi/2.
+		if delta[0] == 0 and delta[1] < 0:
+			temp = -1*temp
+		if delta[0] != 0 :
+			temp = arctan(delta[1]/delta[0])
+		theta = (0.5*pi - temp) * 180/pi
 	HE = abs(heading - theta)
+	# print(theta, heading)
 	if HE > 180:
 		HE = 360 - HE
 	return HE
@@ -55,18 +64,57 @@ def createCandidate(P, l_x, l_y, p_speed, p_head):
 			y2 = l_x[id][i+1]
 			pd = calculatePD((x1,y1), (x2,y2), P)
 			# PDs.append([id,x1,y1,pd])
-			he = calculateHE((x1,y1), (x2,y2), P)
+			he = calculateHE((x1,y1), (x2,y2), p_head)
 			PDs.append([id,x1,y1,he,pd])
-	PDs = np.asanyarray(PDs)
+	PDs = np.asanyarray(PDs,)
 	sind = PDs.argsort(axis=0)
 	PDs = PDs[sind[:,4],:]
+	# print(PDs.dtype)
 	if p_speed < 7.:
 		candidates = PDs[:4,:3].tolist()
 	else:
-		ind = np.where(PDs[:,3] <= 45)[0]
+		arr = np.asarray(PDs[:,3],dtype = np.float32)
+		# print(arr.shape)
+		# print(arr.dtype)
+		ind = np.where(arr <= 45.0)
+		ind = ind[0]
 		PDs = PDs[ind,:]
 		candidates = PDs[:2,:3].tolist()
 	return candidates
+
+def TTP(slot_data, l_x, l_y, p_speed, p_head):
+	p_x, p_y = slot_data
+	# print(p_x)
+	if p_x.shape[0] <2:
+		# print('Not enough points ...')
+		return {}
+	candidates = defaultdict(lambda: [])
+	for i in range(p_x.shape[0]):
+		x = p_x[i]
+		y = p_y[i]
+		candidates[str(x)+','+str(y)] = createCandidate((x,y), l_x, l_y, p_speed[i], p_head[i])
+	return candidates
+
+def MapMatching(p_id, d_t, p_x, p_y, slots, l_x, l_y, p_speed, p_head):
+	x = None
+	cand = defaultdict(lambda: {})
+	print('Creating Candidates ... ')
+	prog = 0.
+	print('Completed: {:.2f}'.format(prog),end=' ')
+	for j,k in enumerate(slots):
+		# print(slots[k])
+		tot = len(slots[k])
+		for j,i in enumerate(slots[k]):
+			ind = np.where((p_id == i) & ((d_t >= slots[k][i][0]) & (d_t <= slots[k][i][-1])))[0]
+			x = TTP((p_x[ind], p_y[ind]), l_x, l_y, p_speed[ind], p_head[ind])
+			cand[k][i] = x
+		# break
+		prog = (j/(float(tot)-1.)) * 100
+		print('\rCompleted: {:.2f}'.format(prog),end = ' ') 
+	print()
+	# print(cand)
+	json.dump(cand,open('candidates.json','w'))
+	# print(x)
 
 if __name__ == '__main__':
 	dat = 'probe_data_map_matching'
@@ -76,6 +124,9 @@ if __name__ == '__main__':
 	l_x, l_y = loadLinkLatLong(dat)
 	p_speed = loadProbeSpeed(dat)
 	p_head = loadProbeHeading(dat)
+	
+	MapMatching(p_id, d_t, p_x, p_y, slots, l_x, l_y, p_speed, p_head)
+
 	# print(p_speed[:10])
 	# print(p_head[:10])
 	# for j,x3 in enumerate(p_x):
