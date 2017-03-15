b01dd843695bbfc2e850fd0786f7a38992c39142
Daksh Gupta <dakshkumargupta@gmail.com>
2017-03-14 19:59:54 -0500

    Updated load_data.py on master to avoid conflict

--- load_data.py
+++ load_data.py
@@ -173,8 +173,7 @@ def loadLinkLength(dir):
 	pickle.dump(dist,open('linkLengths.pckl','wb'))
 	return x, dist	
 
-# 
-def loadLink(dir, ref, nref):
+def loadLink(dir):
 	x,y,z,cat = np.loadtxt(dir+'/Partition6467LinkData.csv', dtype=str, delimiter=',', usecols=(0,1,2,5), unpack=True)
 	dist = np.loadtxt(dir+'/Partition6467LinkData.csv', dtype=float, delimiter=',', usecols=(3,))
 	graph = defaultdict(lambda: [])
